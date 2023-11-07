c90c9b1b3f72039099047d982f5bcb14b80afc2d
LENOVO <a.maghraby96@yahoo.com>
2023-11-07 16:44:42 +0200

    profile

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -7,35 +7,31 @@ import { User } from 'src/infrastructure/entities/user/user.entity';
 import { ApiBearerAuth, ApiConsumes } from '@nestjs/swagger';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { UploadValidator } from 'src/core/validators/upload.validator';
-import { UserInfoResponse } from './dto/response/profile.response';
+    import { UserInfoResponse } from './dto/response/profile.response';
 import { Request } from 'express';
 import { REQUEST } from '@nestjs/core';
 import { JwtAuthGuard } from '../authentication/guards/jwt-auth.guard';
 import { RolesGuard } from '../authentication/guards/roles.guard';
 import { ActionResponse } from 'src/core/base/responses/action.response';
+@ApiBearerAuth()
+@UseGuards(JwtAuthGuard, RolesGuard)
 @Controller()
 export class UserController {
 
   constructor(private readonly userService: UserService,
     @Inject(REQUEST) readonly request: Request,) { }
 
-    @ApiBearerAuth()
- 
-    @UseGuards(JwtAuthGuard, RolesGuard)
+
   @UseInterceptors(ClassSerializerInterceptor, FileInterceptor('file'))
   @ApiConsumes('multipart/form-data')
   
- 
-
 
   @Put('/profile')
-
   async updateProfile(
     @Body() req: UpdateProfileRequest,
     @UploadedFile(new UploadValidator().build()) file: Express.Multer.File,
   ) {
     const user = this.request.user;
-
     req.file = file;
     if (req.file ) await this.userService.updateImage(req);
     user.email = req.email;
