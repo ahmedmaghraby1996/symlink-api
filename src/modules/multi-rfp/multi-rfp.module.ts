1f4f2fdf2f943dbd7e18d6b073a98d9f0f5bef29
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-12-11 22:34:43 +0200

    Add RfpOwnerGuard to MultiRfpModule to guard the owner client of rfp

--- src/modules/multi-rfp/multi-rfp.module.ts
+++ src/modules/multi-rfp/multi-rfp.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { MultiRfpController } from './multi-rfp.controller';
 import { MultiRfpService } from './multi-rfp.service';
 import { RequestForProposalModule } from '../request-for-proposal/request-for-proposal.module';
+import { RfpOwnerGuard } from './guards/rfp_owner.guard';
 
 @Module({
   controllers: [MultiRfpController],
-  providers: [MultiRfpService],
-  exports: [MultiRfpService],
+  providers: [MultiRfpService, RfpOwnerGuard],
+  exports: [MultiRfpService, RfpOwnerGuard],
 })
-export class MultiRfpModule {}
+export class MultiRfpModule { }
