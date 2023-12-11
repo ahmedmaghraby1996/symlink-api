559f5fcb1cd7894cfae0d1f4b1ca7e367cc23657
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-12-11 14:53:32 +0200

    Refactor discussion controller to generate less endpoint

--- src/modules/discussion/discussion.controller.ts
+++ src/modules/discussion/discussion.controller.ts
@@ -1,11 +1,10 @@
-import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from '../authentication/guards/jwt-auth.guard';
 import { RolesGuard } from '../authentication/guards/roles.guard';
-import { CreateMessageDTO } from './dto/create-message.dto';
+import { CreateDiscussionObjectDTO } from './dto/create-discussion-entity.dto';
 import { ApiBearerAuth, ApiHeader, ApiTags } from '@nestjs/swagger';
-import { MessageService } from './message.service';
-import { CreateReplyDTO } from './dto/create-reply.dto';
-import { ReplyService } from './reply.service';
+import { DiscussionService } from './discussion.service';
+import { OptionalMessageQueryDTO } from './dto/optional-message-query.dto';
 
 @ApiBearerAuth()
 @ApiHeader({
@@ -18,48 +17,26 @@ import { ReplyService } from './reply.service';
 @Controller('discussion')
 export class DiscussionController {
     constructor(
-        private readonly messageService: MessageService,
-        private readonly replyService: ReplyService
+        private readonly messageService: DiscussionService,
     ) { }
 
     @Post(':multi_RFP_id/messages')
-    async createMessage(@Param("multi_RFP_id") multi_RFP_id: string, @Body() message: CreateMessageDTO) {
-        return await this.messageService.createMessage(multi_RFP_id, message);
+    async createMessage(
+        @Param("multi_RFP_id") multi_RFP_id: string,
+        @Query() query: OptionalMessageQueryDTO,
+        @Body() message: CreateDiscussionObjectDTO
+    ) {
+        return await this.messageService.createMessage(multi_RFP_id, query, message);
     }
 
-    @Post(':multi_RFP_id/messages/:message_id')
-    async createReplyMessage(
-        @Param("multi_RFP_id") multi_RFP_id: string,
-        @Param("message_id") message_id: string,
-        @Body() reply: CreateReplyDTO
-    ) {
-        return await this.replyService.createReplyMessage(multi_RFP_id, message_id, reply);
-    }
 
     @Get(':multi_RFP_id/messages/:offset/:limit')
     async getMessagesByChunk(
         @Param("multi_RFP_id") multi_RFP_id: string,
+        @Query() query: OptionalMessageQueryDTO,
         @Param("offset") offset: number,
         @Param("limit") limit: number
     ) {
-        return await this.messageService.getMessagesByChunk(multi_RFP_id, offset, limit);
-    }
-
-    @Post(':multi_RFP_id/messages/:reply_id/replies')
-    async createReplyReplyMessage(
-        @Param("multi_RFP_id") multi_RFP_id: string,
-        @Param("reply_id") reply_id: string,
-        @Body() message: CreateReplyDTO
-    ) {
-        return await this.replyService.createReplyReplyMessage(multi_RFP_id, reply_id, message);
-    }
-
-    @Get(':multi_RFP_id/messages/:identifier_id/replies/:offset/:limit')
-    async getRepliesByChunk(
-        @Param("identifier_id") identifierId: string,
-        @Param("offset") offset: number,
-        @Param("limit") limit: number
-    ) {
-        return await this.replyService.getRepliesByChunk(identifierId, offset, limit);
+        return await this.messageService.getItemsByChunk(multi_RFP_id, query, offset, limit);
     }
 }
