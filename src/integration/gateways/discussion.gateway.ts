4d0ad3d3d893f3c322b44a677776d89c9a9f7175
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-12-12 12:23:23 +0200

    Add middleware for RFP discussion privacy and offer gateway functionality

--- src/integration/gateways/discussion.gateway.ts
+++ src/integration/gateways/discussion.gateway.ts
@@ -8,20 +8,31 @@ import { User } from 'src/infrastructure/entities/user/user.entity';
 import { WsJwtAuthGuard } from 'src/modules/authentication/guards/ws-auth.guard';
 import { SocketAuthMiddleware } from './middleware/ws-auth.mw';
 import { ConfigService } from '@nestjs/config';
+import { RfpDiscussionPrivacyMiddleware } from './middleware/ws-rfp-discussion-privacy.mw';
+import { InjectRepository } from '@nestjs/typeorm';
+import { MultiRFP } from 'src/infrastructure/entities/multi-rfp/multi-rfp.entity';
+import { Repository } from 'typeorm';
+import { Offer } from 'src/infrastructure/entities/offer/offer.entity';
 
 @WebSocketGateway({ namespace: Gateways.Discussion.Namespace, cors: { origin: '*' } })
 @UseGuards(WsJwtAuthGuard)
 export class DiscussionGateway {
-    constructor(private configService: ConfigService) { }
+    constructor(
+        private configService: ConfigService,
+        @InjectRepository(MultiRFP) private multiRFPRepository: Repository<MultiRFP>,
+        @InjectRepository(User) private userRepository: Repository<User>,
+        @InjectRepository(Offer) private offersRepository: Repository<Offer>
+    ) { }
 
     @WebSocketServer()
     server: Server;
 
     afterInit(client: Socket) {
-        client.use(SocketAuthMiddleware(this.configService) as any);
+        client.use(SocketAuthMiddleware(this.configService, this.userRepository) as any);
+        client.use(RfpDiscussionPrivacyMiddleware(this.multiRFPRepository, this.offersRepository) as any);
     }
 
-    handleSendMessage(payload: { rfp_id: string, action: string, entity_type: string, entity: Message | Reply }) {
-        this.server.emit(`discussion_${payload.rfp_id}`, payload);
+    handleSendMessage(payload: { multi_RFP: MultiRFP, action: string, entity_type: string, entity: Message | Reply }) {
+        this.server.emit(`discussion_${payload.multi_RFP.id}`, payload);
     }
 }
