a85b55fdd75101c81b8e1950bb0d6793c023c1f6
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-12-18 03:26:39 +0200

    Refactor provider-info.entity.ts and user.entity.ts and setup relations

--- src/infrastructure/entities/provider-info/provider-info.entity.ts
+++ src/infrastructure/entities/provider-info/provider-info.entity.ts
@@ -1,5 +1,5 @@
 import { BaseEntity } from "src/infrastructure/base/base.entity";
-import { Column, Entity, OneToMany, OneToOne } from "typeorm";
+import { Column, Entity, JoinColumn, OneToMany, OneToOne } from "typeorm";
 import { ProviderCertificate } from "./provider-certificate.entity";
 import { ProviderProject } from "./provider-project.entity";
 import { User } from "../user/user.entity";
@@ -7,23 +7,22 @@ import { OwnedEntity } from "src/infrastructure/base/owned.entity";
 
 @Entity()
 
-export class ProviderInfo extends OwnedEntity{
+export class ProviderInfo extends OwnedEntity {
+    @Column({ nullable: true, type: 'text' })
+    educational_info: string
 
+    @OneToMany(() => ProviderCertificate, (providerCertificate) => providerCertificate.provider_info)
+    provider_certificates: ProviderCertificate[]
 
-@Column({nullable:true,type: 'text'})
-educational_info:string
+    @OneToMany(() => ProviderProject, (providerProject) => providerProject.provider_info)
+    provider_projects: ProviderProject[]
 
-@OneToMany(()=>ProviderCertificate, (providerCertificate)=>providerCertificate.provider_info)
-provider_certificates:ProviderCertificate[]
+    @OneToOne(() => User, (user) => user.provider_info)
+    @JoinColumn({ name: 'user_id' })
+    user: User
 
-@OneToMany(()=>ProviderProject, (providerProject)=>providerProject.provider_info)
-provider_projects:ProviderProject[]
-
-@OneToOne(()=>User)
-user:User
-
-constructor(data:Partial<ProviderInfo>){
-    super();
-    Object.assign(this,data)
-}
+    constructor(data: Partial<ProviderInfo>) {
+        super();
+        Object.assign(this, data)
+    }
 }
