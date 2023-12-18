43387489070142fcc70c390d7839e56963f5fe55
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-12-18 13:29:31 +0200

    Add MultiRFP SortyBy enum

---
+++ src/infrastructure/data/enums/multi-rfp-sortby.enum.ts
@@ -0,0 +1,6 @@
+export enum MultiRFPSortBy {
+    PROJECT_NAME = 'project_name',
+    EXPIRATION_DATE = 'expiration_date',
+    REQUEST_FOR_PROPOSAL_STATUS = 'request_for_proposal_status',
+    CREATED_AT = 'created_at',
+}
