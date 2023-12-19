e93ca3c9ca50e5b0d2f11125703def5918c075fa
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-12-19 22:38:09 +0200

    Add file URL transformation to TicketAttachmentResponse

--- src/modules/support-ticket/dto/response/ticket-attachment.response.ts
+++ src/modules/support-ticket/dto/response/ticket-attachment.response.ts
@@ -1,8 +1,9 @@
-import { Expose } from "class-transformer";
+import { Expose, Transform } from "class-transformer";
+import { toUrl } from "src/core/helpers/file.helper";
 
 export class TicketAttachmentResponse {
     @Expose() id: string;
-    @Expose() file_url: string;
+    @Expose() @Transform(({ value }) => toUrl(value)) file_url: string;
     @Expose() file_name: string;
     @Expose() file_type: string;
     @Expose() created_at: Date;
