fbcde62c06a0ff83a8c6ec191726054d9be1a10f
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-12-19 21:49:30 +0200

    Add createTicket endpoint to SupportTicketController

--- src/modules/support-ticket/support-ticket.controller.ts
+++ src/modules/support-ticket/support-ticket.controller.ts
@@ -1,12 +1,19 @@
 import {
+    Body,
+    ClassSerializerInterceptor,
     Controller,
     Post,
+    UploadedFile,
     UseGuards,
+    UseInterceptors,
 } from '@nestjs/common';
 import { JwtAuthGuard } from '../authentication/guards/jwt-auth.guard';
 import { RolesGuard } from '../authentication/guards/roles.guard';
-import { ApiBearerAuth, ApiHeader, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiConsumes, ApiHeader, ApiTags } from '@nestjs/swagger';
 import { SupportTicketService } from './support-ticket.service';
+import { CreateTicketRequest } from './dto/request/create-ticket.request';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { UploadValidator } from 'src/core/validators/upload.validator';
 
 @ApiBearerAuth()
 @ApiHeader({
@@ -18,5 +25,16 @@ import { SupportTicketService } from './support-ticket.service';
 @UseGuards(JwtAuthGuard, RolesGuard)
 @Controller('support-ticket')
 export class SupportTicketController {
-    constructor(private readonly supportTicketService: SupportTicketService ) { }
+    constructor(private readonly supportTicketService: SupportTicketService) { }
+
+    @Post()
+    @UseInterceptors(ClassSerializerInterceptor, FileInterceptor('file'))
+    @ApiConsumes('multipart/form-data')
+    async createTicket(
+        @Body() createTicketRequest: CreateTicketRequest,
+        @UploadedFile(new UploadValidator().build()) file: Express.Multer.File,
+    ) {
+        createTicketRequest.file = file;
+        return this.supportTicketService.createTicket(createTicketRequest);
+    }
 }
