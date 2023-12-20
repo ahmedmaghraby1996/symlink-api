d18b097f3fd61bf160369767fb4c4c029bde5c6a
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-12-20 03:43:41 +0200

    Add Ticket Comment Request and Ticket Comment Response DTOs

--- src/modules/support-ticket/dto/response/ticket-comment.response.ts
+++ src/modules/support-ticket/dto/response/ticket-comment.response.ts
@@ -1,18 +1,16 @@
 import { Expose, Type } from "class-transformer";
-import { SupportTicketStatus } from "src/infrastructure/data/enums/support-ticket-status.enum";
 import { TicketAttachmentResponse } from "./ticket-attachment.response";
+import { SupportTicketResponse } from "./support-ticket.response";
 import { RegisterResponse } from "src/modules/authentication/dto/responses/register.response";
 
-export class SupportTicketResponse {
+export class TicketCommentResponse {
     @Expose() id: string;
-    @Expose() subject: string;
-    @Expose() description: string;
-    @Expose() status: SupportTicketStatus;
-    @Expose() ticket_num: string;
+    @Expose() comment_text: string;
     @Expose() @Type(() => TicketAttachmentResponse) attachment: TicketAttachmentResponse;
     @Expose() created_at: Date;
     @Expose() updated_at: Date;
     @Expose() user_id: string;
+    @Expose() @Type(() => SupportTicketResponse) ticket: SupportTicketResponse;
+    @Expose() ticket_id: string;
     @Expose() @Type(() => RegisterResponse) user: RegisterResponse;
-
 }
