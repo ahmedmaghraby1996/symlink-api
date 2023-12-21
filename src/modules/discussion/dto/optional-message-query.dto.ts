300baeb7b813294a57bb49757072d9a80d3ff212
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-12-21 15:33:16 +0200

    Add ApiPropertyOptional decorator to OptionalMessageQueryDTO

--- src/modules/discussion/dto/optional-message-query.dto.ts
+++ src/modules/discussion/dto/optional-message-query.dto.ts
@@ -1,6 +1,8 @@
+import { ApiPropertyOptional } from "@nestjs/swagger";
 import { IsNotEmpty, IsOptional, IsString } from "class-validator";
 
 export class OptionalMessageQueryDTO {
+    @ApiPropertyOptional()
     @IsOptional()
     @IsString()
     message_id?: string;
