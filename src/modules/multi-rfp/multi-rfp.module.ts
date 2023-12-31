7da69b420c6376d3e0a6da98a003ad43f4ac950d
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-12-31 12:49:57 +0200

    Add FileService to providers in MultiRfpModule

--- src/modules/multi-rfp/multi-rfp.module.ts
+++ src/modules/multi-rfp/multi-rfp.module.ts
@@ -4,10 +4,11 @@ import { MultiRfpService } from './multi-rfp.service';
 import { RequestForProposalModule } from '../request-for-proposal/request-for-proposal.module';
 import { RfpOwnerGuard } from './guards/rfp_owner.guard';
 import { RequestForProposalService } from '../request-for-proposal/request-for-proposal.service';
+import { FileService } from '../file/file.service';
 
 @Module({
   controllers: [MultiRfpController],
-  providers: [MultiRfpService, RfpOwnerGuard, RequestForProposalService],
+  providers: [MultiRfpService, RfpOwnerGuard, RequestForProposalService, FileService],
   exports: [MultiRfpService, RfpOwnerGuard],
 })
 export class MultiRfpModule { }
