d0e899c2ed48c7e16a6789d9ade0473a15b6961c
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-12-31 12:50:43 +0200

    Refactor MultiRFPResponse class to handle multiple RequestForProposalResponse objects

--- src/modules/multi-rfp/dto/multi-rfp.response.ts
+++ src/modules/multi-rfp/dto/multi-rfp.response.ts
@@ -1,3 +1,4 @@
+import { toUrl } from "src/core/helpers/file.helper";
 import { RequestForProposalStatus } from "src/infrastructure/data/enums/request-for-proposal.enum";
 import { RequestForProposalResponse } from "src/modules/request-for-proposal/dto/request-for-proposal.response";
 import { UserInfoResponse } from "src/modules/user/dto/response/profile.response";
@@ -7,15 +8,19 @@ export class MultiRFPResponse{
     request_for_proposal_status: RequestForProposalStatus;
     project_name: string;
 
-    requestForProposalResponse:RequestForProposalResponse;
+    request_for_proposal:RequestForProposalResponse[];
+    
 
     constructor(data: Partial<MultiRFPResponse>) {
         Object.assign(this, data);
         if(data.user!=null){
             this.user = new UserInfoResponse(data.user);
         }
-        if(data.requestForProposalResponse!=null){
-            this.requestForProposalResponse = new RequestForProposalResponse(data.requestForProposalResponse)
+        if(data.request_for_proposal!=null){
+            for (let index = 0; index < data.request_for_proposal.length; index++) {
+                this.request_for_proposal[index].apk_attachment.file_url = toUrl(data.request_for_proposal[index].apk_attachment.file_url);
+                this.request_for_proposal[index] = new RequestForProposalResponse(data.request_for_proposal[index])
+            }
         }
       }
 }
