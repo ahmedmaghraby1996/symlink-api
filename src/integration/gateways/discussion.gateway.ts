fae11e56b45fcd225b86927889b976a9bb1b65e3
ahmedeid6842 <ahmedeid6842@gmail.com>
2024-01-01 14:40:47 +0200

    Add message and reply handling privacy logic to DiscussionGateway

--- src/integration/gateways/discussion.gateway.ts
+++ src/integration/gateways/discussion.gateway.ts
@@ -14,6 +14,7 @@ import { MultiRFP } from 'src/infrastructure/entities/multi-rfp/multi-rfp.entity
 import { Repository } from 'typeorm';
 import { Offer } from 'src/infrastructure/entities/offer/offer.entity';
 import { MessageResponse } from 'src/modules/discussion/dto/response/message.response';
+import { RequestForProposalStatus } from 'src/infrastructure/data/enums/request-for-proposal.enum';
 
 @WebSocketGateway({ namespace: Gateways.Discussion.Namespace, cors: { origin: '*' } })
 @UseGuards(WsJwtAuthGuard)
@@ -33,13 +34,40 @@ export class DiscussionGateway {
         client.use(RfpDiscussionPrivacyMiddleware(this.multiRFPRepository, this.offersRepository) as any);
     }
 
-    handleSendMessage(payload: { multi_RFP: MultiRFP, action: string, entity_type: string, entity: MessageResponse }) {
-        this.server.emit(`discussion_${payload.multi_RFP.id}`, payload);
+    private async emitToAuthorizedSockets(room: string, payload: any) {
+        const connectedSockets: any = this.server.sockets;
+        connectedSockets.forEach(socket => {
+            if (
+                socket.user &&
+                (
+                    socket.user.id === payload.multi_RFP.user_id ||
+                    socket.user.id === payload.multi_RFP.provider_id ||
+                    socket.user.roles.includes('ADMIN')
+                )
+            ) {
+                socket.emit(room, payload);
+            }
+        });
     }
 
-    handleSendReply(payload: { multi_RFP: MultiRFP, action: string, entity_type: string, entity: MessageResponse }) {
+    private async sendEventToRoom(room: string, payload: any) {
+        this.server.emit(room, payload);
+    }
+
+    async handleSendMessage(payload: { multi_RFP: MultiRFP, action: string, entity_type: string, entity: MessageResponse }) {
+        if (payload.multi_RFP.request_for_proposal_status === RequestForProposalStatus.APPROVED) {
+            await this.emitToAuthorizedSockets(`discussion_${payload.multi_RFP.id}`, payload);
+        } else {
+            await this.sendEventToRoom(`discussion_${payload.multi_RFP.id}`, payload);
+        }
+    }
+
+    async handleSendReply(payload: { multi_RFP: MultiRFP, action: string, entity_type: string, entity: MessageResponse }) {
         const message_id = payload.entity.message_id || payload.entity.parent_reply_id;
-        this.server.emit(`message_${message_id}`, payload);
+        if (payload.multi_RFP.request_for_proposal_status === RequestForProposalStatus.APPROVED) {
+            await this.emitToAuthorizedSockets(`message_${message_id}`, payload);
+        } else {
+            await this.sendEventToRoom(`message_${message_id}`, payload);
+        }
     }
-
 }
