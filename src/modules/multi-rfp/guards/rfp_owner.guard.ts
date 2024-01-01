790f16d3bd2dd4824b1d7c18b1b9e580ef62cbbb
ahmedeid6842 <ahmedeid6842@gmail.com>
2024-01-01 14:41:27 +0200

    Refactor RfpOwnerGuard to use TypeORM repository

--- src/modules/multi-rfp/guards/rfp_owner.guard.ts
+++ src/modules/multi-rfp/guards/rfp_owner.guard.ts
@@ -1,16 +1,19 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { MultiRfpService } from '../multi-rfp.service';
+import { InjectRepository } from '@nestjs/typeorm';
+import { MultiRFP } from 'src/infrastructure/entities/multi-rfp/multi-rfp.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class RfpOwnerGuard implements CanActivate {
-    constructor(private multiRfpService:MultiRfpService ) { }
+    constructor(
+        @InjectRepository(MultiRFP) private multiRFPRepository: Repository<MultiRFP>
+    ) { }
 
     async canActivate(context: ExecutionContext): Promise<boolean> {
         const request = context.switchToHttp().getRequest();
         const { multi_RFP_id } = request.params;
-
-        const rfp = await this.multiRfpService.getSingleMultiRFP(multi_RFP_id)
-
+        const rfp = await this.multiRFPRepository.findOne({ where: { id: multi_RFP_id } , relations: ['user'] })
         return rfp.user.id === request.user.id;
     }
 }
